a1fb8f3c8243ff664c173b5e923545f268b1152c
Chenxi Mao <chenxi.mao2013@gmail.com>
2014-07-03 19:27:37 +0800

    CDMA: fix China CDMA card cannnot select APN list issue
    This issue found in China Telecomm operator, the
    PROPERTY_APN_SIM_OPERATOR_NUMERIC value cannot be recognized correctly
    in UICCRecords. This will cause APN list cannot be displayed in setting menu.
    So I refer to SIMRecords implement and re-read IMSI in
    UICC records.
    After apply this patch, the PROPERTY_APN_SIM_OPERATOR_NUMERIC is correct vaule
    and the APN list can be displayed in setting menu.
    
    Change-Id: Ifd4eed5f774cdd9322646a75c14348f9dee6d77d

--- src/java/com/android/internal/telephony/uicc/RuimRecords.java
+++ src/java/com/android/internal/telephony/uicc/RuimRecords.java
@@ -19,7 +19,7 @@ package com.android.internal.telephony.uicc;
 
 import static com.android.internal.telephony.TelephonyProperties.PROPERTY_ICC_OPERATOR_ISO_COUNTRY;
 import static com.android.internal.telephony.TelephonyProperties.PROPERTY_ICC_OPERATOR_NUMERIC;
-
+import static com.android.internal.telephony.TelephonyProperties.PROPERTY_APN_SIM_OPERATOR_NUMERIC;
 import static com.android.internal.telephony.TelephonyProperties.PROPERTY_ICC_OPERATOR_ALPHA;
 import static com.android.internal.telephony.TelephonyProperties.PROPERTY_TEST_CSIM;
 
@@ -141,6 +141,8 @@ public final class RuimRecords extends IccRecords {
         mIccId = null;
 
         mAdnCache.reset();
+
+        setSystemProperty(PROPERTY_APN_SIM_OPERATOR_NUMERIC, null);
 
         // Don't clean up PROPERTY_ICC_OPERATOR_ISO_COUNTRY and
         // PROPERTY_ICC_OPERATOR_NUMERIC here. Since not all CDMA
@@ -682,6 +684,15 @@ public final class RuimRecords extends IccRecords {
     @Override
     protected void onAllRecordsLoaded() {
         if (DBG) log("record load complete");
+
+        String operator = getOperatorNumeric();
+        if (!TextUtils.isEmpty(operator)) {
+            log("onAllRecordsLoaded set 'gsm.apn.sim.operator.numeric' to operator='" +
+                    operator + "'");
+            setSystemProperty(PROPERTY_APN_SIM_OPERATOR_NUMERIC, operator);
+        } else {
+            log("onAllRecordsLoaded empty 'gsm.apn.sim.operator.numeric' skipping");
+        }
 
         setLocaleFromCsim();
         mRecordsLoadedRegistrants.notifyRegistrants(
