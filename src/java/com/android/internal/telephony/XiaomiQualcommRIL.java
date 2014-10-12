7fb5b343eb58c333846dbbb6b866901d0d2c7c19
martincz <martincz@mokeedev.com>
2014-10-12 20:27:34 +0800

    update xiaomi ril
    Change-Id: I0457d3e6b3a267b4274a1fc56e796d156146977f

--- src/java/com/android/internal/telephony/XiaomiQualcommRIL.java
+++ src/java/com/android/internal/telephony/XiaomiQualcommRIL.java
@@ -104,6 +104,11 @@ public class XiaomiQualcommRIL extends RIL implements CommandsInterface {
             case RIL_REQUEST_SETUP_DATA_CALL: ret =  responseSetupDataCall(p); break;
             case RIL_REQUEST_SIM_IO: ret =  responseICC_IO(p); break;
             case RIL_REQUEST_SEND_USSD: ret =  responseVoid(p); break;
+            case RIL_REQUEST_SIM_TRANSMIT_BASIC: ret =  responseICC_IO(p); break;
+            case RIL_REQUEST_SIM_OPEN_CHANNEL: ret  = responseInts(p); break;
+            case RIL_REQUEST_SIM_CLOSE_CHANNEL: ret  = responseVoid(p); break;
+            case RIL_REQUEST_SIM_TRANSMIT_CHANNEL: ret = responseICC_IO(p); break;
+            case RIL_REQUEST_SIM_GET_ATR: ret = responseString(p); break;
             case RIL_REQUEST_CANCEL_USSD: ret =  responseVoid(p); break;
             case RIL_REQUEST_GET_CLIR: ret =  responseInts(p); break;
             case RIL_REQUEST_SET_CLIR: ret =  responseVoid(p); break;
